6698a3d5cb12e0a7525ef6305f16f8466a830252
jln.manns@gmail.com <jln.manns@gmail.com>
2022-10-26 17:27:33 +0200

    compiles in vs, linux and emcc. Cannot load image in browser yet

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,62 @@
-#include "SDL.h" 
-#include "SDL_image.h"
+#include <SDL.h> 
+#include <SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+void mainLoop();
+#else
+#endif
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Surface* surface;
+
+int main(int argc, char* argv[])
 {
-  SDL_Init(SDL_INIT_VIDEO);
-  int imgFlags = IMG_INIT_PNG;
-  IMG_Init(imgFlags);
+	SDL_Init(SDL_INIT_VIDEO);
+	int imgFlags = IMG_INIT_PNG;
+	IMG_Init(imgFlags);
+    SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
+    //surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
+    surface = IMG_Load("map.png");
+    if (surface == NULL) {
+        surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
+        Uint8* pixels = (Uint8*) surface->pixels;
+        for (int i = 0; i < 1048576; i++) {
+            char randomByte = 150 % 255;
+            pixels[i] = randomByte;
+        }
+    }
+    SDL_Texture* screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
-  SDL_Window *window = SDL_CreateWindow(
-    "SDL2Test",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
+#ifdef EMSCRIPTEN
+	emscripten_set_main_loop(mainLoop, 0, 1);
+}
+void mainLoop() {
+#else
+	while (true)
+#endif
+	{
+        //Uint8* pixels = (Uint8*) surface->pixels;
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+        //for (int i = 0; i < 1048576; i++) {
+        //    char randomByte = rand() % 255;
+        //    pixels[i] = randomByte;
+        //}
 
-  SDL_Delay(3000);
+        //if (SDL_MUSTLOCK(surface)) SDL_UnlockSurface(surface);
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+        //SDL_Texture* screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
 
-  return 0;
+        //SDL_RenderClear(renderer);
+        //SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+        //SDL_RenderPresent(renderer);
+
+        //SDL_DestroyTexture(screenTexture);
+	}
 }
